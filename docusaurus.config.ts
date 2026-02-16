5f60950ada5984456c0023f8007b366999f44fbb
Kirill Mokevnin <mokevnin@gmail.com>
2026-02-16 15:32:39 -0500

    Integrate Algolia DocSearch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,33 +9,6 @@ const config: Config = {
   tagline: 'Ответы на частые вопросы и инструкции для студентов',
   favicon: 'img/favicon.ico',
 
-  plugins: [
-    [
-      require.resolve("@cmfcmf/docusaurus-search-local"),
-      {
-        indexDocs: true,
-        indexDocSidebarParentCategories: 2,
-        includeParentCategoriesInPageTitle: false,
-        indexBlog: false,
-        indexPages: false,
-        language: ["ru", "en"],
-        style: undefined,
-        maxSearchResults: 10,
-
-        // lunr.js-specific settings
-        lunr: {
-          tokenizerSeparator: /[\s\-.,!?:;()]+/, // чуть более "мягкое" разделение слов
-          b: 0.6, // лёгкое снижение нормализации по длине, чтобы длинные статьи не терялись
-          k1: 1.2, // чуток усиливаем вклад совпадений редких терминов
-          titleBoost: 6, // совпадения в заголовках по-прежнему самые важные
-          contentBoost: 1.8, // усиливаем вклад основного текста статьи
-          tagsBoost: 3, // снижаем вклад тегов, чтобы они не перегружали выдачу
-          parentCategoriesBoost: 1.5, // навигационные категории влияют мягче
-        }
-      }
-    ],
-  ],
-
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
@@ -88,6 +61,12 @@ const config: Config = {
   ],
 
   themeConfig: {
+    algolia: {
+      appId: '31S4GA91US',
+      apiKey: 'fb627b1a0c425541c758c75bfd47e15c',
+      indexName: 'hexlet_help_pages',
+      contextualSearch: true,
+    },
     // Declare some <meta> tags
     metadata: [
       { name: 'algolia-site-verification', content: '093713AA286F7B7D' },
